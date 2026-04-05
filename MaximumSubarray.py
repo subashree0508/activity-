9c8a21727d52606c875f322bb631928c08a0ec21
SUBASHREE <subashree3565@gmail.com>
2026-04-05 12:22:48 +0530

    Add files via upload

---
+++ MaximumSubarray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSubArray(self, nums):
+        max_sum = nums[0]
+        for i in range(len(nums)):
+            current_sum = 0
+            for j in range(i, len(nums)):
+                current_sum += nums[j]
+                if current_sum > max_sum:
+                    max_sum = current_sum
+        return max_sum
