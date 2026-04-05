75b23c8acf5f7d28d21509d5c43df51be112292b
SUBASHREE <subashree3565@gmail.com>
2026-04-05 12:22:25 +0530

    Add files via upload

---
+++ ProductofArrayExceptSelf.py
@@ -0,0 +1,10 @@
+class Solution:
+    def productExceptSelf(self, nums):
+        result = []
+        for i in range(len(nums)):
+            product = 1
+            for j in range(len(nums)):
+                if i != j:
+                    product *= nums[j]
+            result.append(product)
+        return result
